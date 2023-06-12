a9012350728232a820549f0dd88da17de1f68cc9
Vyekk <s20699@pjwstk.edu.pl>
2023-06-13 01:28:21 +0200

    Added simple functionality of basket

--- mp_basket/js/Basket.js
+++ mp_basket/js/Basket.js
@@ -9,7 +9,18 @@ class Basket {
         this.basket = [];
     }
     removeProduct(productNumber) {
-        this.basket.splice(productNumber-1);
+        this.basket.splice(productNumber - 1);
+    }
+    getBusketSummary() {
+        return this.basket.map((product, i) => {
+            return {
+                id: i + 1,
+                text: `${product.name} - ${product.price.toFixed(0)}zł`,
+            };
+        });
+    }
+    getBusketTotalPrice() {
+        return this.basket.reduce((prev, curr) => (prev += curr.price), 0);
     }
 }
 
--- mp_basket/js/main.js
+++ mp_basket/js/main.js
@@ -1,10 +1,40 @@
 import Basket from "./Basket.js";
 import Product from "./Product.js";
 
-const basket = new Basket();
-const tomato = new Product("Pomidor", 12.50);
-const watermelon = new Product("Arbuz", 23.00);
-const cucumber = new Product("Ogórek", 8.99);
+const productBtns = document.querySelectorAll("[data-name]");
+const uiBasket = document.querySelector(".basket-list");
+const buyAllBtn = document.querySelector(".btn-buy-all");
 
-basket.addProduct(tomato);
-console.log(basket);
+const basket = new Basket();
+
+const createUiBasket = () => {
+    uiBasket.innerText = "";
+    for (const { id, text } of basket.getBusketSummary()) {
+        const productLi = document.createElement("li");
+        productLi.innerText = text;
+        productLi.dataset.id = id;
+        uiBasket.appendChild(productLi);
+    }
+    buyAllBtn.innerText = `Złóż zamówienie za ${basket
+        .getBusketTotalPrice()
+        .toFixed(2)}zł`;
+    buyAllBtn.disabled = basket.getBusketTotalPrice() === 0;
+};
+
+const addProductToBasket = (event) => {
+    const productName = event.target.dataset.name;
+    const productPrice = Number(event.target.dataset.price);
+    const newProduct = new Product(productName, productPrice);
+    basket.addProduct(newProduct);
+    createUiBasket();
+};
+
+productBtns.forEach((button) => {
+    button.addEventListener("click", addProductToBasket);
+});
+
+buyAllBtn?.addEventListener("click", () => {
+    alert(`Złożono zamówienie na wszystkie wybrane przez Ciebie produkty`);
+    basket.resetBasket();
+    createUiBasket();
+});
