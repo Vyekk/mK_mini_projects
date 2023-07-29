e96fcc5010b368c24f49d8a176a523bda3911eb5
Vyekk <koluchkon@gmail.com>
2023-07-29 03:29:30 +0200

    Created constants file

---
+++ mp_node_encrypt/constants.js
@@ -0,0 +1,7 @@
+const ALGORITHM = 'aes-192-cbc';
+const SALT = 'NASJKHJD*g827y581hjbIUGAS*&FDGY*ASUG*&FGVFBASJIn92hjnknmk#%!@#$(I*Y&! TYjkandkjasnd278Y&*YH@h@#%!&%%(!@&^&*!@%*G!@U(HBIBbasidbasdh9124yhb&@YUG@B%BVy812g';
+
+module.exports = {
+  ALGORITHM,
+  SALT,
+};
