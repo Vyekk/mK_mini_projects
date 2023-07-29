e2140a6d9aed50395944159ad767dff426836ab5
Vyekk <koluchkon@gmail.com>
2023-07-29 03:30:10 +0200

    Migrated constants to another file and add hmac

--- mp_node_encrypt/cipher.js
+++ mp_node_encrypt/cipher.js
@@ -2,10 +2,8 @@ const { promisify } = require('util');
 const scrypt = promisify(require('crypto').scrypt);
 const randomBytes = promisify(require('crypto').randomBytes);
 const { createCipheriv, createDecipheriv } = require('crypto');
-
-const ALGORITHM = 'aes-192-cbc';
-const SALT =
-  'NASJKHJD*g827y581hjbIUGAS*&FDGY*ASUG*&FGVFBASJIn92hjnknmk#%!@#$(I*Y&! TYjkandkjasnd278Y&*YH@h@#%!&%%(!@&^&*!@%*G!@U(HBIBbasidbasdh9124yhb&@YUG@B%BVy812g';
+const { createHmac } = require('crypto');
+const { SALT, ALGORITHM } = require('./constants');
 
 async function createEncryptedMessage(fileContent, password) {
   const key = await scrypt(password, SALT, 24);
@@ -13,20 +11,29 @@ async function createEncryptedMessage(fileContent, password) {
   const cipher = createCipheriv(ALGORITHM, key, iv);
   let encrypted = cipher.update(fileContent, 'utf-8', 'hex');
   encrypted += cipher.final('hex');
+  const hmac = createHmac('sha512', SALT)
+    .update(fileContent)
+    .digest('hex');
   return {
     encrypted,
     iv: iv.toString('hex'),
+    hmac,
   };
 }
 
 async function decryptMessage(fileContent, password) {
-  const { encrypted, iv: ivHex } = JSON.parse(fileContent);
+  const { encrypted, iv: ivHex, hmac } = JSON.parse(fileContent);
   const key = await scrypt(password, SALT, 24);
   const iv = Buffer.from(ivHex, 'hex');
-
   const decipher = createDecipheriv(ALGORITHM, key, iv);
   let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
   decrypted += decipher.final('utf-8');
+  const newHmac = createHmac('sha512', SALT)
+    .update(decrypted)
+    .digest('hex');
+  if (newHmac !== hmac) {
+    throw new Error('Hmac is incorrect');
+  }
   return decrypted;
 }
 
