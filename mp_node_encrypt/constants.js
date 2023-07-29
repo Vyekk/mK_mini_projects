88224c01f4daf613c166fdcd61809962954cab1f
Vyekk <koluchkon@gmail.com>
2023-07-29 04:12:14 +0200

    Changed and added diff salt to hash and encrypt

--- mp_node_encrypt/constants.js
+++ mp_node_encrypt/constants.js
@@ -1,7 +1,9 @@
 const ALGORITHM = 'aes-192-cbc';
-const SALT = 'NASJKHJD*g827y581hjbIUGAS*&FDGY*ASUG*&FGVFBASJIn92hjnknmk#%!@#$(I*Y&! TYjkandkjasnd278Y&*YH@h@#%!&%%(!@&^&*!@%*G!@U(HBIBbasidbasdh9124yhb&@YUG@B%BVy812g';
+const ENCRYPT_SALT = 'NASJKHJD*g827y581hjbIUGAS*&FDGY*ASUG*&FGVFBASJIn92hjnknmk#%!@#$(I*Y&! TYjkandkjasnd278Y&*YH@h@#%!&%%(!@&^&*!@%*G!@U(HBIBbasidbasdh9124yhb&@YUG@B%BVy812g';
+const HASH_SALT = 'JASKLJ@OIKJIO!J@JK!@NKJN !KNNKJ@HEJK HJJKEH!JKEHJKJEDHJ!JI@EHIHBNSJIDNIUASHDAOLSH OLASDH LU@gheahsdjkabdyug2 &*OGHUI@GHUJVB hjsbduyag g2e gbjB@UYHJEGY*IUOQ@eg8oghlasbkjhdbbO@& G@UIBjkABdi;AGw8po@TG*&@UEIB l;.';
 
 module.exports = {
   ALGORITHM,
-  SALT,
+  ENCRYPT_SALT,
+  HASH_SALT,
 };
