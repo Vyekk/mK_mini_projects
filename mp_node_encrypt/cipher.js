5bea5a32491695098d2f3f90c3b0677cfdc042a8
Vyekk <koluchkon@gmail.com>
2023-07-29 04:12:32 +0200

    Changed salts

--- mp_node_encrypt/cipher.js
+++ mp_node_encrypt/cipher.js
@@ -3,15 +3,15 @@ const scrypt = promisify(require('crypto').scrypt);
 const randomBytes = promisify(require('crypto').randomBytes);
 const { createCipheriv, createDecipheriv } = require('crypto');
 const { createHmac } = require('crypto');
-const { SALT, ALGORITHM } = require('./constants');
+const { ENCRYPT_SALT, HASH_SALT, ALGORITHM } = require('./constants');
 
 async function createEncryptedMessage(fileContent, password) {
-  const key = await scrypt(password, SALT, 24);
+  const key = await scrypt(password, ENCRYPT_SALT, 24);
   const iv = await randomBytes(16);
   const cipher = createCipheriv(ALGORITHM, key, iv);
   let encrypted = cipher.update(fileContent, 'utf-8', 'hex');
   encrypted += cipher.final('hex');
-  const hmac = createHmac('sha512', SALT)
+  const hmac = createHmac('sha512', HASH_SALT)
     .update(fileContent)
     .digest('hex');
   return {
@@ -23,12 +23,12 @@ async function createEncryptedMessage(fileContent, password) {
 
 async function decryptMessage(fileContent, password) {
   const { encrypted, iv: ivHex, hmac } = JSON.parse(fileContent);
-  const key = await scrypt(password, SALT, 24);
+  const key = await scrypt(password, ENCRYPT_SALT, 24);
   const iv = Buffer.from(ivHex, 'hex');
   const decipher = createDecipheriv(ALGORITHM, key, iv);
   let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
   decrypted += decipher.final('utf-8');
-  const newHmac = createHmac('sha512', SALT)
+  const newHmac = createHmac('sha512', HASH_SALT)
     .update(decrypted)
     .digest('hex');
   if (newHmac !== hmac) {
