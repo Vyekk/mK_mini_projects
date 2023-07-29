4b819d7436870967b4a1185f500f6ee397c1dbcf
Vyekk <koluchkon@gmail.com>
2023-07-29 04:18:02 +0200

    extract code to function createHash

--- mp_node_encrypt/cipher.js
+++ mp_node_encrypt/cipher.js
@@ -5,15 +5,19 @@ const { createCipheriv, createDecipheriv } = require('crypto');
 const { createHmac } = require('crypto');
 const { ENCRYPT_SALT, HASH_SALT, ALGORITHM } = require('./constants');
 
+function createHash(text, salt) {
+  return createHmac('sha512', salt)
+    .update(text)
+    .digest('hex');
+}
+
 async function createEncryptedMessage(fileContent, password) {
   const key = await scrypt(password, ENCRYPT_SALT, 24);
   const iv = await randomBytes(16);
   const cipher = createCipheriv(ALGORITHM, key, iv);
   let encrypted = cipher.update(fileContent, 'utf-8', 'hex');
   encrypted += cipher.final('hex');
-  const hmac = createHmac('sha512', HASH_SALT)
-    .update(fileContent)
-    .digest('hex');
+  const hmac = createHash(fileContent, HASH_SALT);
   return {
     encrypted,
     iv: iv.toString('hex'),
@@ -28,9 +32,7 @@ async function decryptMessage(fileContent, password) {
   const decipher = createDecipheriv(ALGORITHM, key, iv);
   let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
   decrypted += decipher.final('utf-8');
-  const newHmac = createHmac('sha512', HASH_SALT)
-    .update(decrypted)
-    .digest('hex');
+  const newHmac = createHash(decrypted, HASH_SALT);
   if (newHmac !== hmac) {
     throw new Error('Hmac is incorrect');
   }
